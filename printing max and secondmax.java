f0775715ed14e0bbd50965b8a0c6d1589a247339
prasanna kothoju <134475079+prasannakothoju@users.noreply.github.com>
2025-10-28 18:36:36 +0530

    Add method to find max and second max in array
    Implement a method to find the maximum and second maximum values in an array.

---
+++ printing max and secondmax.java
@@ -0,0 +1,38 @@
+/// Second Largest Element in an Array without sorting
+
+import java.util.*;
+import java.util.Scanner;
+
+public class A{
+    
+    //method to find max and second max
+    public static void findSecondMax(int[] arr){
+        //declaring max values
+        int max = Integer.MIN_VALUE;
+        int secondmax = Integer.MIN_VALUE;
+        
+        //“Is this number bigger than the biggest one we’ve seen so far?”
+        for(int num : arr){
+            if(num>max){ //if yes
+                secondmax = max; //we make the old largest become the second largest.Because now we found someone even bigger!
+                max = num; //This number is the new largest
+            }
+            
+            else if(num>secondmax && num != max){
+                secondmax = num; //If the number is not the biggest,
+                     //but it’s still bigger than the current second biggest,
+                              //then it becomes the new second largest.
+            }
+        }
+        System.out.println("Max of the array is: " + max);
+        System.out.println("Secondmax of the array is: " + secondmax);
+    }
+    
+    public static void main(String[] args){
+    //take an array and initialize it
+    int[] arr = {8,3,6,4,5,3,9};
+    int n = arr.length;
+    
+    findSecondMax(arr);
+    }
+}
